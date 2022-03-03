83d6e6b82f102dabe0220d5ff795fe7c1f151032
Zach <zach303@msn.com>
2022-03-03 07:51:09 -0800

    button navigation uses state machine

--- Assets/ButtonNavigation.cs
+++ Assets/ButtonNavigation.cs
@@ -6,58 +6,44 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(MenuStack))]
 public class ButtonNavigation : MonoBehaviour
 {
-    [SerializeField] bool mouseOnly;
     [SerializeField] bool useCursorInitially;
+    [SerializeField] bool mouseOnly;
 
     MenuStack stack;
     EventSystem eventSystem;
-    bool usingCursor;
+
+    StateMachine<NavState> stateMachine;
+    NavState mouseState, buttonState;
 
     void Start()
     {
         stack = GetComponent<MenuStack>();
         eventSystem = EventSystem.current;
-        usingCursor = useCursorInitially;
+
+        mouseState = new NavState(MouseUpdate);
+        buttonState = new NavState(ButtonUpdate);
+        stateMachine = new StateMachine<NavState>(useCursorInitially ? mouseState : buttonState);
     }
 
     void Update()
     {
-        // Enable cursor on mouse input.
-        bool mouseMoved = Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0;
-        bool mousePressed = Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1);
-        if (!usingCursor && (mouseMoved || mousePressed))
-        {
-            usingCursor = true;
-            Cursor.visible = true;   
-        }
+        // Update current state.
+        stateMachine.GetState().Update();
 
-        // Disable cursor on keyboard or gamepad input.
-        bool mouseButtons = Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1);
-        bool keyboardInput = Input.anyKeyDown && !Input.GetButtonDown("Cancel") && !mouseButtons;
-        if (usingCursor && keyboardInput && !mouseOnly)
+        // Regardless of state, keep track of the previously selected button. This allows the
+        // keyboard button selection to choose the correct button when, for example, the menu is
+        // popped and the previously menu is activated.
+        if (!stack.Empty() && eventSystem.currentSelectedGameObject != null)
         {
-            usingCursor = false;
-            Cursor.visible = false;  
+            stack.Peek().GetComponent<MenuBase>()
+                .previouslySelectedButton = eventSystem.currentSelectedGameObject;
         }
+    }
 
-        // Select a button if one is not already selected.
-        if (!usingCursor && !stack.Empty() && !eventSystem.currentSelectedGameObject)
-        {
-            MenuBase menuBase = stack.Peek().GetComponent<MenuBase>();
-            if (menuBase.previouslySelectedButton != null)
-            {
-                // Select the previously selected button.
-                eventSystem.SetSelectedGameObject(menuBase.previouslySelectedButton);
-            }
-            else
-            {
-                // Select the initial button.
-                eventSystem.SetSelectedGameObject(menuBase.initialButton);
-            }
-        }
-
+    void MouseUpdate()
+    {
         // Select buttons that the cursor is hovering over.
-        if (usingCursor && !stack.Empty())
+        if (!stack.Empty())
         {
             // Get the pointer data.
             PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
@@ -87,13 +73,51 @@ public class ButtonNavigation : MonoBehaviour
             }
         }
 
-        // Regardless of cursor, keep track of the previously selected button. This allows the
-        // keyboard button selection to choose the correct button when, for example, the menu is
-        // popped and the previously menu is activated.
-        if (!stack.Empty() && eventSystem.currentSelectedGameObject != null)
+        // Switch to button state on keyboard or gamepad input.
+        bool mouseButtons = Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1);
+        bool keyboardInput = Input.anyKeyDown && !Input.GetButtonDown("Cancel") && !mouseButtons;
+        if (keyboardInput && !mouseOnly)
         {
-            stack.Peek().GetComponent<MenuBase>()
-                .previouslySelectedButton = eventSystem.currentSelectedGameObject;
+            Cursor.visible = false;
+            stateMachine.SetState(buttonState);
         }
+    }
+
+    void ButtonUpdate()
+    {
+        // Select a button if one is not already selected.
+        if (!stack.Empty() && !eventSystem.currentSelectedGameObject)
+        {
+            MenuBase menuBase = stack.Peek().GetComponent<MenuBase>();
+            if (menuBase.previouslySelectedButton != null)
+            {
+                // Select the previously selected button.
+                eventSystem.SetSelectedGameObject(menuBase.previouslySelectedButton);
+            }
+            else
+            {
+                // Select the initial button.
+                eventSystem.SetSelectedGameObject(menuBase.initialButton);
+            }
+        }
+
+        // Switch to mouse state on mouse input.
+        bool mouseMoved = Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0;
+        bool mousePressed = Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1);
+        if (mouseMoved || mousePressed)
+        {
+            Cursor.visible = true;
+            stateMachine.SetState(mouseState);
+        }
+    }
+
+    class NavState : StateBase
+    {
+        public NavState(StateCallback update)
+        {
+            Update = update;
+        }
+
+        public StateCallback Update = () => {};
     }
 }
